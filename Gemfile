5a3140d614f75998de07c5985d36fb91c4504046
Matthew Sullivan <matthew@msull92.com>
2017-09-03 19:40:18 -0500

    Simplify

--- Gemfile
+++ Gemfile
@@ -1,10 +1,4 @@
 source "https://rubygems.org"
 
-# use awesomeprint to easily debug
-gem "awesome_print"
-
 # use Google cloud language to make sentiment analysis
 gem "google-cloud-language"
-
-# use rufus-scheduler to time when emails are sent and processed
-gem "rufus-scheduler"
--- main.rb
+++ main.rb
@@ -1,79 +1,69 @@
-require "awesome_print"
 require "csv"
 require "date"
 require "google/cloud/language"
 require "json/ext"
-require "rufus-scheduler"
-
-ENV["TZ"] = "America/Chicago"
 
 language_client  = Google::Cloud::Language.new(project: ENV["GOOGLE_CLOUD_PROJECT_ID"])
-scheduler = Rufus::Scheduler.new
+nodes = []
+date_groups = {}
 
-scheduler.every "1d" do
-  nodes = []
-  date_groups = {}
-
-  CSV.foreach("external-data/responses.csv", headers: true) do |row|
-    body = row["body"].downcase.strip
-    date = unless row["date"] == nil
-      Date.parse(row["date"]).strftime("%m/%d/%y")
-    end
-
-    nodes << body
-    date_groups[date] = [] if date_groups[date] == nil
-    date_groups[date] << body
+CSV.foreach("external-data/responses.csv", headers: true) do |row|
+  body = row["body"].downcase.strip
+  date = unless row["date"] == nil
+    Date.parse(row["date"]).strftime("%m/%d/%y")
   end
 
-  combinations = date_groups.map{|key, value|
-    value.product(value).map{|v|
-      v.sort
-    }.reject{|c|
-      c.empty?
-    }.uniq.map{|v|
-      {
-        source: nodes.index(v[0]),
-        target: nodes.index(v[1])
-      }
-    }
-  }.flatten.sort_by{|v|
-    v[:source]
-  }
-
-  mapped_links = combinations.map{|v|
-    v[:frequency] = combinations.group_by{|x|
-      "#{x[:source]}.#{x[:target]}"
-    }["#{v[:source]}.#{v[:target]}"].length
-
-    v
-  }.uniq.reject{|v|
-    v[:source] == v[:target]
-  }
-
-  mapped_nodes = nodes.map{|node|
-    sentiment_score = language_client.document(node).sentiment.score
-    rounded_score = if sentiment_score <= -0.25
-      0
-    elsif sentiment_score >= 0.25
-      2
-    else
-      1
-    end
-
-    {
-      name: node,
-      group: rounded_score
-    }
-  }
-
-  matrix = {
-    nodes: mapped_nodes,
-    links: mapped_links
-  }.to_json
-
-  File.open("data/co-occurrence-map.json","w") do |file|
-    file.write(matrix)
-  end
+  nodes << body
+  date_groups[date] = [] if date_groups[date] == nil
+  date_groups[date] << body
 end
 
-scheduler.join
+combinations = date_groups.map{|key, value|
+  value.product(value).map{|v|
+    v.sort
+  }.reject{|c|
+    c.empty?
+  }.uniq.map{|v|
+    {
+      source: nodes.index(v[0]),
+      target: nodes.index(v[1])
+    }
+  }
+}.flatten.sort_by{|v|
+  v[:source]
+}
+
+mapped_links = combinations.map{|v|
+  v[:frequency] = combinations.group_by{|x|
+    "#{x[:source]}.#{x[:target]}"
+  }["#{v[:source]}.#{v[:target]}"].length
+
+  v
+}.uniq.reject{|v|
+  v[:source] == v[:target]
+}
+
+mapped_nodes = nodes.map{|node|
+  sentiment_score = language_client.document(node).sentiment.score
+  rounded_score = if sentiment_score <= -0.25
+    0
+  elsif sentiment_score >= 0.25
+    2
+  else
+    1
+  end
+
+  {
+    name: node,
+    group: rounded_score
+  }
+}
+
+matrix = {
+  nodes: mapped_nodes,
+  links: mapped_links
+}.to_json
+
+File.open("data/co-occurrence-map.json","w") do |file|
+  file.write(matrix)
+end
